17defa5d293593d9d644c42c79bbf86dbd2477ea
SpaghettiRebel <slavadem3106@gmail.com>
2026-06-11 00:30:08 +0300

    producer in work

--- public_api/src/public_api/infrastructure/kafka/producer.py
+++ public_api/src/public_api/infrastructure/kafka/producer.py
@@ -4,3 +4,14 @@ from confluent_kafka import Producer
 class KafkaEventPublisher:
     def __init__(self, producer):
         producer = Producer()
+
+    @staticmethod
+    def delivery_callback(err, msg):
+        topic = msg.topic()
+        key = msg.key().decode('utf-8')
+        value = msg.value().decode('utf-8')
+
+        if err:
+            print(f'ERROR: Message failed delivery: {err}')
+        else:
+            print(f'Produced event to topic {topic}: key = {key:12} value = {value:12}')
